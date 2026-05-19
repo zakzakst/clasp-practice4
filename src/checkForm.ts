1f2c8ed1496b8a0038af5bec09d78c967fdc16e1
zakzakst <e1433104@gmail.com>
2026-05-19 17:39:13 +0900

    fix

--- src/checkForm.ts
+++ src/checkForm.ts
@@ -9,14 +9,9 @@ const getCheckData = (): any[][] | undefined => {
   return [data[0], data[1], data[selectedRow - 1]];
 };
 
-const formDataToSheet = (data: FormData) => {
+const formDataToSheet = (data: { index: number; value: string }[]) => {
   // TODO: 入力データをシートに反映（データ表示時に選択行をHTML側で保持して、こちらの関数の引数に連携する必要がある）
-  // data.entries().forEach(([name, value]) => {
-  //   Logger.log(name, value);
-  // });
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const data2 = getSheetData_(sheet);
-  return data2;
+  Logger.log(data);
 };
 
 const showCheckForm_ = () => {
