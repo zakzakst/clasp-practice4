11e9abe648f753cd2dae181366c3a9fe08f5e930
zakzakst <e1433104@gmail.com>
2026-05-19 18:05:39 +0900

    fix

--- src/checkForm.ts
+++ src/checkForm.ts
@@ -1,17 +1,27 @@
 import { getSheetData_ } from "@spreadsheet/getSheetData";
 import { showSidebar_ } from "@spreadsheet/showSidebar";
 
-const getCheckData = (): any[][] | undefined => {
+const getCheckData = (): { rowIndex: number; values: any[][] } | undefined => {
   const sheet = SpreadsheetApp.getActiveSheet();
   if (!sheet) return;
   const data = getSheetData_(sheet);
   const selectedRow = sheet.getActiveCell().getRow();
-  return [data[0], data[1], data[selectedRow - 1]];
+  return {
+    rowIndex: selectedRow,
+    values: [data[0], data[1], data[selectedRow - 1]],
+  };
 };
 
-const formDataToSheet = (data: { index: number; value: string }[]) => {
-  // TODO: 入力データをシートに反映（データ表示時に選択行をHTML側で保持して、こちらの関数の引数に連携する必要がある）
-  Logger.log(data);
+const formDataToSheet = (
+  rowIndex: number,
+  inputData: { colIndex: number; value: string }[],
+) => {
+  const sheet = SpreadsheetApp.getActiveSheet();
+  inputData.forEach((data) => {
+    if (data.value) {
+      sheet.getRange(rowIndex, data.colIndex).setValue(data.value);
+    }
+  });
 };
 
 const showCheckForm_ = () => {
